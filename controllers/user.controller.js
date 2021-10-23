196df3289ab9e19e1e22d80200887b0c2981f5d4
Luigi Morel <morelmiles@gmail.com>
2021-10-23 23:40:30 +0300

    user controller finish

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,72 +1,105 @@
-import mongoose from "mongoose";
-import express from "express";
-import userSchema from "./../models/user";
-
-const router = express.Router();
+const User = require("./../models/user");
+const errorHandler = require("./../helpers/errorFormatter");
+const extend = require("lodash/extend");
 
 //Create the user and save the details to the database
 const createUser = async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    emailAddress,
-    password,
-    address,
-    apartment,
-    country,
-    postalCode,
-    phoneNumber,
-  } = req.body;
-
-  const newUser = new userSchema({
-    firstName,
-    lastName,
-    emailAddress,
-    password,
-    address,
-    apartment,
-    country,
-    postalCode,
-    phoneNumber,
-  });
-
+  const user = new User(req.body);
   try {
-    await newUser.save();
-
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
+    await user.save();
+    return res.status(200).json({
+      message: "Successfully created an account!",
+    });
+  } catch (err) {
+    return res.status(400).json({ error: errorHandler.getErrorMessage(err) });
   }
 };
 
-//Gets a single product from the database
+/**
+ * Please note that not all of these routes should be customer facing,
+ * Are for the admin side
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * @param {*} id
+ * @returns
+ */
 
-const getProduct = async (req, res) => {
-  const { id } = req.params;
+//List a user by the ID
 
+const userByID = async (req, res, next, id) => {
   try {
-    const post = await userSchema.findById(id);
-
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(404).json({ message: errror.message });
+    let user = await User.findById(id);
+    if (!user)
+      return res.status("400").json({
+        error: "User not found",
+      });
+    req.profile = user;
+    next();
+  } catch (err) {
+    return res.status("400").json({ error: "Could not retrieve user" });
   }
 };
 
-//Fectch all products
-const getProducts = async (req, res) => {
+// List all the users in the database
+const listUsers = async (req, res) => {
   try {
-    const products = await userSchema.find();
-
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
+    let users = await User.find().select(
+      "name email address apartment country postCode phoneNumber email updated created"
+    );
+    res.json(users);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
   }
 };
 
-//Updates a single user
-// const updateUser =async (req, res) => {
-//     const {id} = req.params
+// Read the user by an ID
+//Do not return the salt and the hashed password
+const readUserById = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
 
-//     const
-// }
+// Update a single user
+const updateUserById = async (req, res) => {
+  try {
+    let user = req.profile;
+    user = extend(user, req.body);
+    user.updated = Date.now();
+    await user.save();
+    user.hashed_password = undefined;
+    user.salt = undefined;
+    res.json(user);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
+
+//Delete user from the database
+const deleteUserById = async (req, res) => {
+  try {
+    let user = req.profile;
+    let deletedUser = await user.remove();
+    deletedUser.hashed_password = undefined;
+    deletedUser.salt = undefined;
+    res.json(deletedUser);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
+
+export default {
+  createUser,
+  userByID,
+  listUsers,
+  readUserById,
+  updateUserById,
+  deleteUserById,
+};
