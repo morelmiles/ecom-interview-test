faed7aaa7156ac4e9ddf7d55ba6064249ea9e435
Luigi Morel <morelmiles@gmail.com>
2021-10-24 21:38:45 +0300

    Change import of the helpers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("./../models/user");
+const User = require("../models/user.model");
 const errorHandler = require("./../helpers/errorFormatter");
 const extend = require("lodash/extend");
 
