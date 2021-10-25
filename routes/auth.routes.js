193090efd1d1481ae6789306d6badc79ee100174
Luigi Morel <morelmiles@gmail.com>
2021-10-25 14:03:22 +0300

    Change the controller import

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
-import authCtrl from "../controllers/auth.controller";
+const authCtrl = require("../controllers/auth.controller");
+
 const router = express.Router();
 router.route("/auth/signin").post(authCtrl.signin);
 router.route("/auth/signout").get(authCtrl.signout);
