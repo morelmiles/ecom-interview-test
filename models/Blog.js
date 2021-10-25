1ef42c633a393d54809afdbf687eb73886ae674c
Luigi Morel <morelmiles@gmail.com>
2021-10-25 13:11:19 +0300

    Add the blog model

---
+++ models/Blog.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const BlogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: "Title is required",
+    },
+    tags: {
+      type: String,
+      required: "Tags are required",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.Model("Blog", BlogSchema);
