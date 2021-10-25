ac615fe009b12d3339a41c2b0ad08a438c73002e
Luigi Morel <morelmiles@gmail.com>
2021-10-25 10:19:00 +0300

    Add order handler

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,5 +1,5 @@
-const Order = require("../models/order.model");
-const Product = require("./../models/product.model");
+const Order = require("../models/Order");
+const Product = require("../models/Product");
 
 const getOrders = (req, res) => {
   Order.find({})
