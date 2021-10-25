a666f210923a621129ab187372c1c0daf3d7ef5d
Luigi Morel <morelmiles@gmail.com>
2021-10-25 14:03:56 +0300

    Update the routes for thre user model

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,15 +4,15 @@ const userCtrl = require("./../controllers/user.controller");
 const router = express.Router();
 
 //Routes for the users
-router.route("/api/users").post(userCtrl.createUser);
-router.route("/api/users").get(userCtrl.listUsers);
-router.route("/api/users/:userId").get(userCtrl.readUserById);
-router.route("/api/users/:userId").put(userCtrl.updateUserById);
-router.route("/api/users/:userId").delete(userCtrl.deleteUserById);
+router.route("/api/v1/users").post(userCtrl.createUser);
+router.route("/api/v1/users").get(userCtrl.listUsers);
+router.route("/api/v1/users/:userId").get(userCtrl.readUserById);
+router.route("/api/v1/users/:userId").put(userCtrl.updateUserById);
+router.route("/api/v1/users/:userId").delete(userCtrl.deleteUserById);
 
 //Require authorization so as to do the following operations
 router
-  .route("/api/users/:userId")
+  .route("/api/v1/users/:userId")
   .get(authCtrl.requireSignin, userCtrl.read)
   .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
   .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
