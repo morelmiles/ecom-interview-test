6f4c637382603e9252cf66f985eb04dfdddc3dd1
Luigi Morel <morelmiles@gmail.com>
2021-10-25 15:11:02 +0300

    Add the resource routes

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const compress = require("compression");
-const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+const orderRoutes = require("./routes/order.routes");
+const productRoutes = require("./routes/product.routes");
+const blogRoutes = require("./routes/blog.routes");
+const categoryRoutes = require("./routes/category.routes");
+const clientRoutes = require("./routes/client.routes");
+const roleRoutes = require("./routes/role.routes");
+const userRoutes = require("./routes/user.routes");
+
 require("dotenv").config();
 
 const app = express();
@@ -14,8 +23,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(express.json({ extended: true }));
 app.use(compress());
+app.use(morgan("dev"));
 
-//Database connection
 mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -29,6 +38,16 @@ db.once("open", () => {
   console.log("Connected successfully");
 });
 
+//Routes for the different resources
+app.use("/api/v1/orders", orderRoutes);
+app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/blog", blogRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/client", clientRoutes);
+app.use("/api/v1/role", roleRoutes);
+app.use("/api/v1/user", userRoutes);
+
+//Handle the unauthourized request error for the user route
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: err.name + ": " + err.message });
@@ -36,6 +55,7 @@ app.use((err, req, res, next) => {
     res.status(400).json({ error: err.name + ": " + err.message });
     console.log(err);
   }
+  next();
 });
 
 //Listen to the requests
