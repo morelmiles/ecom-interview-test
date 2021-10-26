de786517fba1b66ad16f13a3ac5cf2baade79bd8
Luigi Morel <morelmiles@gmail.com>
2021-10-26 03:54:50 +0300

    Change crud operations naming

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -3,9 +3,23 @@ const clientCtrl = require("../controllers/client.controller");
 const router = express.Router();
 
 //Routes for the client model
-router.route("/").post(clientCtrl.createClient);
-router.route("/").get(clientCtrl.getClients);
 
-router.route("/:clientId").get(clientCtrl.getClient);
-router.route("/:clientId").put(clientCtrl.updateClient);
-router.route("/:clientId").delete(clientCtrl.deleteClient);
+router.post("/", (req, res) => {
+  clientCtrl.createClient;
+});
+
+router.get("/", (req, res) => {
+  clientCtrl.getClients;
+});
+
+router.get("/:clientId", (req, res) => {
+  clientCtrl.getClient;
+});
+
+router.put("/:clientId", (req, res) => {
+  clientCtrl.updateClient;
+});
+
+router.delete("/:clientId", (req, res) => {
+  clientCtrl.deleteClient;
+});
